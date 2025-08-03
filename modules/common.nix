92a98e4aa046a83506deee017b75f562452ee0aa
Nick Jones <nick@dischord.org>
2025-08-03 16:02:36 +0100

    Rename Flake dir

--- modules/common.nix
+++ modules/common.nix
@@ -32,7 +32,7 @@
       enable = true;
       clean.enable = true;
       clean.extraArgs = "--keep-since 4d --keep 3";
-      flake = "/home/nick/src/nixos/";
+      flake = "/home/nick/src/flake/";
     };
   };
 
