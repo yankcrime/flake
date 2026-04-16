d0aea98c57e6727a218472e082b6bacb1bd6d67c
Nick Jones <nick@dischord.org>
2026-04-16 10:17:30 +0100

    Add rain disk stuff

--- hosts/rain/hardware-configuration.nix
+++ hosts/rain/hardware-configuration.nix
@@ -10,10 +10,16 @@
   boot.extraModulePackages = [ ];
 
   boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
 
   swapDevices = [ ];
 
   networking.useDHCP = lib.mkDefault true;
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
+      fsType = "ext4";
+    };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
